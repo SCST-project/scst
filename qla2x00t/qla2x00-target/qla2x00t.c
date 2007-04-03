3b449d4705d278e21a47159328e172d9f684a864
Vladislav Bolkhovitin <vst@vlnb.net>
2007-04-03 14:42:47 +0000

    Copyrights updated, "-g" added in DEBUG mode
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@104 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- qla2x00t/qla2x00-target/qla2x00t.c
+++ qla2x00t/qla2x00-target/qla2x00t.c
@@ -1,7 +1,7 @@
 /*
  *  qla2x00t.c
  *  
- *  Copyright (C) 2004-2006 Vladislav Bolkhovitin <vst@vlnb.net>
+ *  Copyright (C) 2004-2007 Vladislav Bolkhovitin <vst@vlnb.net>
  *                          Leonid Stoljar
  *                          Nathaniel Clark <nate@misrule.us>
  *
