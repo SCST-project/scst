f01c5a935511e08c0b4b7a8e69fb993d982a8eca
Vladislav Bolkhovitin <vst@vlnb.net>
2009-10-22 12:33:45 +0000

    Fix incorrect WARN_ON()
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@1238 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- scst/src/scst_priv.h
+++ scst/src/scst_priv.h
@@ -375,7 +375,7 @@ static inline int scst_exec_req(struct scsi_device *sdev,
 	return scsi_execute_async(sdev, cmd, cmd_len, data_direction, (void *)sgl,
 		    bufflen, nents, timeout, retries, privdata, done, gfp);
 #elif !defined(SCSI_EXEC_REQ_FIFO_DEFINED)
-	WARN_ON();
+	WARN_ON(1);
 	return -1;
 #else
 	return scsi_execute_async_fifo(sdev, cmd, cmd_len, data_direction,
