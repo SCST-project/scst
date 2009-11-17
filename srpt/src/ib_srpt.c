9d65e4fe05903b69c9058c6567aa4e333f62a18e
Bart Van Assche <bvanassche@acm.org>
2009-11-17 18:00:23 +0000

    Removed a BUILD_BUG_ON() statement that was too restrictive.
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@1343 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- srpt/src/ib_srpt.c
+++ srpt/src/ib_srpt.c
@@ -1146,8 +1146,6 @@ static int srpt_build_cmd_rsp(struct srpt_rdma_ch *ch,
 	int sense_data_len;
 	int resp_len;
 
-	BUILD_BUG_ON((sizeof(*sense) % 4) != 0);
-
 	sense_data_len = (s_key == NO_SENSE) ? 0 : sizeof(*sense);
 	resp_len = sizeof(*srp_rsp) + sense_data_len;
 
