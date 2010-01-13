e90930ed6d340bf5864603c79b24420219027cc0
Bart Van Assche <bvanassche@acm.org>
2010-01-13 13:13:03 +0000

    Made sure that none of the EXTRACHECKS_BUG_ON(), EXTRACHECKS_WARN_ON() or EXTRACHECKS_WARN_ON_ONCE() macros trigger a compiler warning about an empty controlled statement in a release or perf build.
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@1458 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- scst/include/scst_debug.h
+++ scst/include/scst_debug.h
@@ -75,9 +75,9 @@
 #define EXTRACHECKS_WARN_ON(a)		WARN_ON(a)
 #define EXTRACHECKS_WARN_ON_ONCE(a)	WARN_ON_ONCE(a)
 #else
-#define EXTRACHECKS_BUG_ON(a)
-#define EXTRACHECKS_WARN_ON(a)
-#define EXTRACHECKS_WARN_ON_ONCE(a)
+#define EXTRACHECKS_BUG_ON(a)		do { } while (0)
+#define EXTRACHECKS_WARN_ON(a)		do { } while (0)
+#define EXTRACHECKS_WARN_ON_ONCE(a)	do { } while (0)
 #endif
 
 #define TRACE_NULL           0x00000000
