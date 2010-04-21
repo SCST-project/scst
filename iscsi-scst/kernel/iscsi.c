eb05cb541ec465c837b69ba44fee5aeb670985f4
Vladislav Bolkhovitin <vst@vlnb.net>
2010-04-21 17:56:48 +0000

    Merge of IET r228: add module version and description
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@1641 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- iscsi-scst/kernel/iscsi.c
+++ iscsi-scst/kernel/iscsi.c
@@ -3772,4 +3772,6 @@ static void __exit iscsi_exit(void)
 module_init(iscsi_init);
 module_exit(iscsi_exit);
 
+MODULE_VERSION(ISCSI_VERSION_STRING);
 MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("SCST iSCSI Target");
