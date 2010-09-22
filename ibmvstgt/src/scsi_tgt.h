fec7afefda288159d2a6e2e86c08b8e2eddd453a
Bart Van Assche <bvanassche@acm.org>
2010-09-22 18:03:20 +0000

    More cleanup.
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@2166 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- ibmvstgt/src/scsi_tgt.h
+++
@@ -1,3 +0,0 @@
-/*
- * SCSI target definitions
- */
--- ibmvstgt/src/scsi_tgt_priv.h
+++
@@ -1,5 +0,0 @@
-/* tmp - will replace with SCSI logging stuff */
-#define eprintk(fmt, args...)					\
-do {								\
-	printk("%s(%d) " fmt, __func__, __LINE__, ##args);	\
-} while (0)
