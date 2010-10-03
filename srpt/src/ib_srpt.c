c42c4159edc0845d22dad9acfd586bc428d734e8
Bart Van Assche <bvanassche@acm.org>
2010-10-03 07:26:03 +0000

    Fixed a checkpatch complaint.
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@2343 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- srpt/src/ib_srpt.c
+++ srpt/src/ib_srpt.c
@@ -2021,7 +2021,7 @@ static void srpt_process_completion(struct ib_cq *cq,
 				    struct srpt_rdma_ch *ch,
 				    enum scst_exec_context context)
 {
-	struct ib_wc* const wc = ch->wc;
+	struct ib_wc *const wc = ch->wc;
 	int i, n;
 
 	EXTRACHECKS_WARN_ON(cq != ch->cq);
