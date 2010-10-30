f775812ffc60ef2723b1c9b113d7d4b7036e0ae2
Bart Van Assche <bvanassche@acm.org>
2010-10-30 07:33:51 +0000

    Fixed a sparse complaint: preprocessor directives inside macro arguments are not allowed.
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@2492 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- scst/src/scst_lib.c
+++ scst/src/scst_lib.c
@@ -2030,10 +2030,11 @@ static void scst_adjust_sg(struct scst_cmd *cmd, struct scatterlist *sg,
 
 	l = 0;
 	for (i = 0, j = 0; i < *sg_cnt; i++, j++) {
-		TRACE_DBG("i %d, j %d, sg_cnt %d, sg %p, page_link %lx", i, j,
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 24)
+		TRACE_DBG("i %d, j %d, sg_cnt %d, sg %p, page_link %lx", i, j,
 			*sg_cnt, sg, sg[j].page_link);
 #else
+		TRACE_DBG("i %d, j %d, sg_cnt %d, sg %p, page_link %lx", i, j,
 			*sg_cnt, sg, 0UL);
 #endif
 		if (unlikely(sg_is_chain(&sg[j]))) {
