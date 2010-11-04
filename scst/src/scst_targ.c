4a80d3c0a5945575d625b5d22c4f04ca256115c8
Vladislav Bolkhovitin <vst@vlnb.net>
2010-11-04 13:55:38 +0000

    Merge of the trunk's r2580:
    Let's be safe and alway do barrier
    
    
    
    git-svn-id: http://svn.code.sf.net/p/scst/svn/branches/2.0.0.x@2594 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- scst/src/scst_targ.c
+++ scst/src/scst_targ.c
@@ -3775,7 +3775,7 @@ static int scst_translate_lun(struct scst_cmd *cmd)
 	TRACE_ENTRY();
 
 	/* See comment about smp_mb() in scst_suspend_activity() */
-	__scst_get(1);
+	__scst_get();
 
 	if (likely(!test_bit(SCST_FLAG_SUSPENDED, &scst_flags))) {
 		struct list_head *sess_tgt_dev_list_head =
@@ -4354,7 +4354,7 @@ static int scst_mgmt_translate_lun(struct scst_mgmt_cmd *mcmd)
 	      (long long unsigned int)mcmd->lun);
 
 	/* See comment about smp_mb() in scst_suspend_activity() */
-	__scst_get(1);
+	__scst_get();
 
 	if (unlikely(test_bit(SCST_FLAG_SUSPENDED, &scst_flags) &&
 		     !test_bit(SCST_FLAG_SUSPENDING, &scst_flags))) {
