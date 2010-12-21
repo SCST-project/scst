414221d1c8f5418c3c37f05a98b4720e71313e59
Bart Van Assche <bvanassche@acm.org>
2010-12-21 18:53:35 +0000

    Updated my e-mail address in copyright statements and README files (merged r3175 from the trunk).
    git-svn-id: http://svn.code.sf.net/p/scst/svn/branches/2.0.0.x@3176 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- srpt/src/ib_srpt.c
+++ srpt/src/ib_srpt.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2006 - 2009 Mellanox Technology Inc.  All rights reserved.
+ * Copyright (C) 2008 - 2010 Bart Van Assche <bvanassche@acm.org>.
  * Copyright (C) 2008 Vladislav Bolkhovitin <vst@vlnb.net>
- * Copyright (C) 2008 - 2010 Bart Van Assche <bart.vanassche@gmail.com>
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
