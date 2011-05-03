dbdc2386b6ab6a2b35fd6826748b8112060d8adf
Vladislav Bolkhovitin <vst@vlnb.net>
2011-05-03 20:51:39 +0000

    MultiP must be 1 and HiSup 0. Dejavu, I've already fixed it.
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@3435 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- scst/src/dev_handlers/scst_vdisk.c
+++ scst/src/dev_handlers/scst_vdisk.c
@@ -1577,11 +1577,11 @@ static void vdisk_exec_inquiry(struct scst_cmd *cmd)
 		if (virt_dev->removable)
 			buf[1] = 0x80;      /* removable */
 		buf[2] = 5; /* Device complies to SPC-3 */
-		buf[3] = 0x12;	/* HiSup + data in format specified in SPC */
+		buf[3] = 0x02;	/* Data in format specified in SPC */
 		if (cmd->tgtt->fake_aca)
 			buf[3] |= 0x20;
 		buf[4] = 31;/* n - 4 = 35 - 4 = 31 for full 36 byte data */
-		buf[6] = 1; /* MultiP 1 */
+		buf[6] = 0x10; /* MultiP 1 */
 		buf[7] = 2; /* CMDQUE 1, BQue 0 => commands queuing supported */
 
 		/*
