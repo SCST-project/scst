e47c6eb768e9ef31937f1ca888192047737b573a
Bart Van Assche <bvanassche@acm.org>
2011-05-08 11:55:43 +0000

    scst.h: Avoid that including scst.h before <linux/slab.h> triggers a warning about a preprocessor symbol redefinition.
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@3441 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- scst/include/scst.h
+++ scst/include/scst.h
@@ -536,7 +536,7 @@ enum scst_exec_context {
 /*************************************************************
  ** Kernel cache creation helper
  *************************************************************/
-#ifndef KMEM_CACHE
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 22)
 #define KMEM_CACHE(__struct, __flags) kmem_cache_create(#__struct,\
 	sizeof(struct __struct), __alignof__(struct __struct),\
 	(__flags), NULL, NULL)
