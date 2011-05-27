7004bdfb7522286253ebf63f596ff066b96b1e6f
Bart Van Assche <bvanassche@acm.org>
2011-05-27 10:37:09 +0000

    Removed a superfluous line in scst.h (Backported r3484 from the trunk).
    git-svn-id: http://svn.code.sf.net/p/scst/svn/branches/2.1.0.x@3485 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- scst/include/scst.h
+++ scst/include/scst.h
@@ -37,7 +37,6 @@
 #elif defined(RHEL_MAJOR) && RHEL_MAJOR -0 <= 5
 #error The SCST sysfs interface is not supported on RHEL 5. Please run make enable_proc.
 #elif LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 26)
-#elif defined(RHEL_MAJOR) && RHEL_MAJOR -0 <= 5
 #error The SCST sysfs interface is supported from kernel version 2.6.26 on. Please run make enable_proc.
 #endif
 
