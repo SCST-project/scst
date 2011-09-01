43d576aa4843ec763eaab48a88e16af0089f60c3
Vladislav Bolkhovitin <vst@vlnb.net>
2011-09-01 20:15:42 +0000

    Changelogs updated
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@3842 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- scst_local/scst_local.c
+++ scst_local/scst_local.c
@@ -84,8 +84,8 @@ static unsigned long scst_local_trace_flag = SCST_LOCAL_DEFAULT_LOG_FLAGS;
 #define scsi_bufflen(cmd) ((cmd)->request_bufflen)
 #endif
 
-#define SCST_LOCAL_VERSION "1.0.0"
-static const char *scst_local_version_date = "20100910";
+#define SCST_LOCAL_VERSION "3.0"
+static const char *scst_local_version_date = "20110901";
 
 /* Some statistics */
 static atomic_t num_aborts = ATOMIC_INIT(0);
