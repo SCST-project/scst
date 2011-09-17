2dc3be5a42a8b854d2d92c6fb959db876bb16ea3
Bart Van Assche <bvanassche@acm.org>
2011-09-17 16:20:48 +0000

    Merge trunk r3632 properly
    git-svn-id: http://svn.code.sf.net/p/scst/svn/branches/2.1.0.x@3861 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- srpt/src/ib_srpt.c
+++ srpt/src/ib_srpt.c
@@ -3042,7 +3042,7 @@ static int srpt_perform_rdmas(struct srpt_rdma_ch *ch,
 				   ioctx->ioctx.index);
 			msleep(1000);
 		}
-		while (atomic_read(&ch->state) != CH_DISCONNECTING &&
+		while (atomic_read(&ch->state) != CH_RELEASING &&
 		       !ioctx->rdma_aborted) {
 			PRINT_INFO("Waiting until RDMA abort finished [%d]",
 				   ioctx->ioctx.index);
