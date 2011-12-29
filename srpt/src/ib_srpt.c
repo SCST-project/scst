a8ddbf4545bcfd33438067227e78dd5e5b8061b6
Bart Van Assche <bvanassche@acm.org>
2011-12-29 13:32:57 +0000

    ib_srpt: Correct version number
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@4040 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- srpt/src/ib_srpt.c
+++ srpt/src/ib_srpt.c
@@ -58,7 +58,7 @@
 
 /* Name of this kernel module. */
 #define DRV_NAME		"ib_srpt"
-#define DRV_VERSION		"2.1.0-pre"
+#define DRV_VERSION		"3.0.0-pre"
 #define DRV_RELDATE		"(not yet released)"
 #if defined(CONFIG_SCST_DEBUG) || defined(CONFIG_SCST_TRACING)
 /* Flags to be used in SCST debug tracing statements. */
