922a8f8178515acef62b5e40d134aef38a1a2229
Bart Van Assche <bvanassche@acm.org>
2014-01-04 14:06:02 +0000

    ibmvstgt: Add #include <linux/module.h> in libsrp.c
    Include <linux/module.h> in order to fix the following errors:
    
    ibmvstgt/src/libsrp.c:477:20: error: expected declaration specifiers or ?...? before string constant
     MODULE_DESCRIPTION("SCSI RDMA Protocol lib functions");
                        ^
    ibmvstgt/src/libsrp.c:478:15: error: expected declaration specifiers or ?...? before string constant
     MODULE_AUTHOR("FUJITA Tomonori");
                   ^
    ibmvstgt/src/libsrp.c:479:16: error: expected declaration specifiers or ?...? before string constant
     MODULE_LICENSE("GPL");
                    ^
    
    Signed-off-by: Sebastian Herbszt <herbszt@gmx.de>
    
    
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@5184 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- ibmvstgt/src/libsrp.c
+++ ibmvstgt/src/libsrp.c
@@ -19,6 +19,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA
  */
+#include <linux/module.h>
 #include <linux/err.h>
 #include <linux/slab.h>
 #include <linux/kfifo.h>
