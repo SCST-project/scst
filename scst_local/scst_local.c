523fe730963d7513779194e4726788f766e620e5
Vladislav Bolkhovitin <vst@vlnb.net>
2014-06-13 05:03:17 +0000

    Forgotten versions updated
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@5597 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- scst_local/scst_local.c
+++ scst_local/scst_local.c
@@ -88,7 +88,7 @@ static unsigned long scst_local_trace_flag = SCST_LOCAL_DEFAULT_LOG_FLAGS;
 #define scsi_bufflen(cmd) ((cmd)->request_bufflen)
 #endif
 
-#define SCST_LOCAL_VERSION "3.0"
+#define SCST_LOCAL_VERSION "3.1"
 static const char *scst_local_version_date = "20110901";
 
 /* Some statistics */
