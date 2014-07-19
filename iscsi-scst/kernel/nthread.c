726be647695cf7edb312df4b3fd7621b08c1fb85
Vladislav Bolkhovitin <vst@vlnb.net>
2014-07-19 03:54:33 +0000

    iscsi-scst: Handle data buffers with non-zero offset correctly
    Start at the proper offset in the receive buffer if sg[0].offset != 0.
    Return the proper data to the initiator if sg[0].offset != 0.
    
    This patch reworks trunk r5281, "iscsi-scst: fix offset
    calculation", February 13, 2014.
    
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    
    
    
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@5683 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- iscsi-scst/kernel/nthread.c
+++ iscsi-scst/kernel/nthread.c
@@ -1396,16 +1396,24 @@ retry:
 	sg_size = size;
 
 	if (sg != write_cmnd->rsp_sg) {
+		/*
+		 * Data scatterlist. It is assumed that only the first element
+		 * has a non-zero offset and that all elements except the
+		 * first and the last have a length that is equal to
+		 * PAGE_SIZE.
+		 */
 		offset = conn->write_offset + sg[0].offset;
 		idx = offset >> PAGE_SHIFT;
-		if (offset + sg[0].offset >= PAGE_SIZE)
-			offset += sg[0].offset;
 		offset &= ~PAGE_MASK;
 		length = min(size, (int)PAGE_SIZE - offset);
 		TRACE_WRITE("write_offset %d, sg_size %d, idx %d, offset %d, "
 			"length %d", conn->write_offset, sg_size, idx, offset,
 			length);
 	} else {
+		/*
+		 * Response scatterlist. No assumptions are made about the
+		 * offset nor about the length of scatterlist elements.
+		 */
 		idx = 0;
 		offset = conn->write_offset;
 		while (offset >= sg[idx].length) {
