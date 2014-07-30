bfef652278d05a0d82933d57ef1e4497082314c0
Vladislav Bolkhovitin <vst@vlnb.net>
2014-07-30 02:27:10 +0000

    Minor logging improvements
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@5706 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- scst/src/scst_lib.c
+++ scst/src/scst_lib.c
@@ -5093,8 +5093,11 @@ static void scst_complete_request_sense(struct scst_cmd *req_cmd)
 	if (scsi_status_is_good(req_cmd->status) && (len > 0) &&
 	    scst_sense_valid(buf)) {
 		TRACE(TRACE_SCSI|TRACE_MGMT_DEBUG, "REQUEST SENSE %p returned "
-			"valid sense", req_cmd);
+			"valid sense (orig cmd %s)", req_cmd, orig_cmd->op_name);
 		PRINT_BUFF_FLAG(TRACE_SCSI|TRACE_MGMT_DEBUG, "Sense", buf, len);
+		if (!scst_no_sense(buf))
+			PRINT_WARNING("REQUEST SENSE returned NO SENSE (orig "
+				"cmd %s)", orig_cmd->op_name);
 		scst_alloc_set_sense(orig_cmd, scst_cmd_atomic(req_cmd),
 			buf, len);
 	} else {
