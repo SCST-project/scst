9249ca54144f3a37aeb9534b00fa8955e620d971
Bart Van Assche <bvanassche@acm.org>
2014-09-03 11:00:55 +0000

    scst_vdisk: Follow-up for r5754
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@5755 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- scst/src/dev_handlers/scst_vdisk.c
+++ scst/src/dev_handlers/scst_vdisk.c
@@ -5256,7 +5256,7 @@ static void vdisk_bio_set_hoq(struct bio *bio)
 {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 36) ||			\
 	defined(RHEL_MAJOR) &&						\
-	(RHEL_MAJOR -0 > 6 || RHEL_MAJOR -0 == 6 && RHEL_MINOR -0 == 0)
+	(RHEL_MAJOR -0 > 6 || RHEL_MAJOR -0 == 6 && RHEL_MINOR -0 > 0)
 	bio->bi_rw |= REQ_SYNC;
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 29)
 	bio->bi_rw |= 1 << BIO_RW_SYNCIO;
@@ -5265,7 +5265,7 @@ static void vdisk_bio_set_hoq(struct bio *bio)
 #endif
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 36) ||			\
 	defined(RHEL_MAJOR) &&						\
-	(RHEL_MAJOR -0 > 6 || RHEL_MAJOR -0 == 6 && RHEL_MINOR -0 == 0)
+	(RHEL_MAJOR -0 > 6 || RHEL_MAJOR -0 == 6 && RHEL_MINOR -0 > 0)
 	bio->bi_rw |= REQ_META;
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 1, 0)
 	/*
