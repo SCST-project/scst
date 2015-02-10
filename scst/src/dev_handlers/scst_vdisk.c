a565e4881957690e5a871d5f9b291b5efeb973d8
Bart Van Assche <bvanassche@acm.org>
2015-02-10 08:12:58 +0000

    scst_vdisk: Source code comment spellig fixes (merge r6022:6024 from trunk)
    git-svn-id: http://svn.code.sf.net/p/scst/svn/branches/iser@6033 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- scst/src/dev_handlers/scst_vdisk.c
+++ scst/src/dev_handlers/scst_vdisk.c
@@ -144,7 +144,7 @@ struct scst_vdisk_dev {
 
 	/*
 	 * This lock can be taken on both SIRQ and thread context, but in
-	 * all cases for each particular instance it's taken consistenly either
+	 * all cases for each particular instance it's taken consistently either
 	 * on SIRQ or thread context. Mix of them is forbidden.
 	 */
 	spinlock_t flags_lock;
@@ -3302,7 +3302,7 @@ static int vdisk_sup_vpd(uint8_t *buf, struct scst_cmd *cmd,
 	*p++ = 0x86; /* extended inquiry */
 	if (cmd->dev->type == TYPE_DISK) {
 		*p++ = 0xB0; /* block limits */
-		*p++ = 0xB1; /* block device charachteristics */
+		*p++ = 0xB1; /* block device characteristics */
 		if (virt_dev->thin_provisioned) {
 			*p++ = 0xB2; /* thin provisioning */
 		}
