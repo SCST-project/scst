4117f30c0679d8464661d9f1edcaa7c7ac8c2dbf
Bart Van Assche <bvanassche@acm.org>
2015-06-10 16:01:24 +0000

    scst: Allow compiling iSER/SRP against OFED/MLNX_OFED
    Both OFED/MLNX_OFED compat and scst define file_inode.
    This causes a compilation error. Fix this by detecting
    that backports are in effect.
    
    Signed-off-by: Yan Burman <yanb@mellanox.com>
    
    
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@6236 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- scst/include/scst.h
+++ scst/include/scst.h
@@ -278,7 +278,8 @@ static inline void hex2bin(u8 *dst, const char *src, size_t count)
 }
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 9, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(3, 9, 0) && \
+	!defined(CONFIG_COMPAT_KERNEL_3_12)
 /*
  * See also patch "new helper: file_inode(file)" (commit ID
  * 496ad9aa8ef448058e36ca7a787c61f2e63f0f54).
