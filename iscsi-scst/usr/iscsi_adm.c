ad38432332eda3c0ff1acd5d2affe79c3b1d80c4
Bart Van Assche <bart.vanassche@sandisk.com>
2015-06-17 00:27:41 +0000

    iscsiadm: Fix strerror() argument
    Detected by Coverity.
    
    
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@6379 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- iscsi-scst/usr/iscsi_adm.c
+++ iscsi-scst/usr/iscsi_adm.c
@@ -147,7 +147,7 @@ static int iscsid_request_send(int fd, struct iscsi_adm_req *req)
 	if (ret != sizeof(*req)) {
 		err = (ret < 0) ? -errno : -EIO;
 		fprintf(stderr, "%s failed: written %d, to write %d, "
-			"error: %s\n", __func__, ret, err, strerror(err));
+			"error: %s\n", __func__, ret, err, strerror(-err));
 	} else
 		err = 0;
 
@@ -173,7 +173,7 @@ static int iscsid_response_recv(int fd, struct iscsi_adm_req *req, void *rsp_dat
 	if (ret != sizeof(rsp) + sizeof(*req)) {
 		err = (ret < 0) ? -errno : -EIO;
 		fprintf(stderr, "readv failed: read %d instead of %d (%s)\n",
-			 ret, (int)(sizeof(rsp) + sizeof(*req)), strerror(err));
+			ret, (int)(sizeof(rsp) + sizeof(*req)), strerror(-err));
 	} else
 		err = rsp.err;
 
@@ -182,7 +182,7 @@ static int iscsid_response_recv(int fd, struct iscsi_adm_req *req, void *rsp_dat
 		if (ret != rsp_data_sz) {
 			err = (ret < 0) ? -errno : -EIO;
 			fprintf(stderr, "read failed: read %d instead of %d (%s)\n",
-				 ret, (int)rsp_data_sz, strerror(err));
+				 ret, (int)rsp_data_sz, strerror(-err));
 		}
 	}
 
