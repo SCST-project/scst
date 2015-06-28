c74a2449c5c0381c9e0305a713db08cdada770ac
Bart Van Assche <bart.vanassche@sandisk.com>
2015-06-28 08:50:33 -0700

    Return ENOTSUPP instead of ENOSYS for operations that are not supported
    ENOSYS must only be used inside system calls.
    
    Signed-off-by: Bart Van Assche <bart.vanassche@sandisk.com>

--- scst/src/scst_proc.c
+++ scst/src/scst_proc.c
@@ -1404,7 +1404,7 @@ static ssize_t scst_proc_scsi_tgt_write(struct file *file,
 	TRACE_ENTRY();
 
 	if (vtt->tgtt->write_proc == NULL) {
-		res = -ENOSYS;
+		res = -ENOTSUPP;
 		goto out;
 	}
 
@@ -1559,7 +1559,7 @@ static ssize_t scst_proc_scsi_dev_handler_write(struct file *file,
 	TRACE_ENTRY();
 
 	if (dev_type->write_proc == NULL) {
-		res = -ENOSYS;
+		res = -ENOTSUPP;
 		goto out;
 	}
 
