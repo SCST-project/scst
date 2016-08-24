3c6e21704fab8fdd07c92fdfaeb90c5e88fac326
Vladislav Bolkhovitin <vst@vlnb.net>
2016-08-24 02:05:03 +0000

    iscsi: convert recently added BUG_ON() to EXTRACHECKS_BUG_ON()
    This check is rather debug check, so should belong to debug build only
    
    
    
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@6980 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- iscsi-scst/kernel/iscsi.c
+++ iscsi-scst/kernel/iscsi.c
@@ -2035,7 +2035,7 @@ static int scsi_cmnd_start(struct iscsi_cmnd *req)
 	atomic_inc(&session->active_cmds);
 	req->dec_active_cmds = 1;
 
-	sBUG_ON(session->scst_sess == NULL);
+	EXTRACHECKS_BUG_ON(session->scst_sess == NULL);
 
 	scst_cmd = scst_rx_cmd(session->scst_sess,
 		(uint8_t *)&req_hdr->lun, sizeof(req_hdr->lun),
