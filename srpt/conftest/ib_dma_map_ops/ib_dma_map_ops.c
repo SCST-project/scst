8730c9c5a688b74460ef5cc675b294d424d37f44
Bart Van Assche <bart.vanassche@sandisk.com>
2017-04-14 20:46:50 +0000

    ib_srpt: Fix two configuration tests
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@7129 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- srpt/conftest/ib_dma_map_ops/ib_dma_map_ops.c
+++ srpt/conftest/ib_dma_map_ops/ib_dma_map_ops.c
@@ -1,11 +1,9 @@
 #include <linux/module.h>
 #include <rdma/ib_verbs.h>
 
-static struct ib_device *dev;
-
 static int modinit(void)
 {
-	return dev->dma_ops != NULL;
+	return ib_dma_mapping_error(NULL, 0) != 0;
 }
 
 module_init(modinit);
