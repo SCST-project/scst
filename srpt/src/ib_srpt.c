bbaf688a0e9855f10f76a3cc81741bb5a2771861
Bart Van Assche <bart.vanassche@sandisk.com>
2017-08-27 21:40:23 +0000

    ib_srpt: Build fix for kernels before v4.12
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@7220 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- srpt/src/ib_srpt.c
+++ srpt/src/ib_srpt.c
@@ -589,7 +589,7 @@ static void srpt_mgmt_method_get(struct srpt_port *sp, struct ib_mad *rq_mad,
 static void srpt_mad_send_handler(struct ib_mad_agent *mad_agent,
 				  struct ib_mad_send_wc *mad_wc)
 {
-#ifdef HAVE_RDMA_DESTROY_AH
+#if HAVE_RDMA_DESTROY_AH
 	rdma_destroy_ah(mad_wc->send_buf->ah);
 #else
 	ib_destroy_ah(mad_wc->send_buf->ah);
@@ -665,7 +665,7 @@ static void srpt_mad_recv_handler(struct ib_mad_agent *mad_agent,
 	ib_free_send_mad(rsp);
 
 err_rsp:
-#ifdef HAVE_RDMA_DESTROY_AH
+#if HAVE_RDMA_DESTROY_AH
 	rdma_destroy_ah(ah);
 #else
 	ib_destroy_ah(ah);
