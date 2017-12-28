56e27f4125d0f6b41d49bc61f991c4f0d0578f0b
Bart Van Assche <bvanassche@acm.org>
2017-12-28 18:43:15 +0000

    qla2x00t: Convert init_timer() into setup_timer() calls
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@7293 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- qla2x00t/qla_os.c
+++ qla2x00t/qla_os.c
@@ -325,10 +325,8 @@ struct scsi_transport_template *qla2xxx_transport_vport_template = NULL;
 __inline__ void
 qla2x00_start_timer(scsi_qla_host_t *vha, void *func, unsigned long interval)
 {
-	init_timer(&vha->timer);
+	init_timer(&vha->timer, func, (unsigned long)vha);
 	vha->timer.expires = jiffies + interval * HZ;
-	vha->timer.data = (unsigned long)vha;
-	vha->timer.function = (void (*)(unsigned long))func;
 	add_timer(&vha->timer);
 	vha->timer_active = 1;
 }
