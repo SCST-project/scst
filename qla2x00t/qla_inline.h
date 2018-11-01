4af98c3bcc123834abb2360cd57ba7a5db4693fe
Bart Van Assche <bvanassche@acm.org>
2018-11-01 04:13:47 +0000

    qla2x00t: Convert init_timer() into setup_timer() calls
    git-svn-id: http://svn.code.sf.net/p/scst/svn/branches/3.3.x@7547 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- qla2x00t/qla_inline.h
+++ qla2x00t/qla_inline.h
@@ -171,10 +171,9 @@ done:
 static inline void
 qla2x00_init_timer(srb_t *sp, unsigned long tmo)
 {
-	init_timer(&sp->u.iocb_cmd.timer);
+	setup_timer(&sp->u.iocb_cmd.timer, qla2x00_sp_timeout,
+		    (unsigned long)sp);
 	sp->u.iocb_cmd.timer.expires = jiffies + tmo * HZ;
-	sp->u.iocb_cmd.timer.data = (unsigned long)sp;
-	sp->u.iocb_cmd.timer.function = qla2x00_sp_timeout;
 	add_timer(&sp->u.iocb_cmd.timer);
 	sp->free = qla2x00_sp_free;
 }
