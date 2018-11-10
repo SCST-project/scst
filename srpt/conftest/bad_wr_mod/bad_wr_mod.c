9b4a6a8823558e03292610dffdfb99c1e49b0f08
Bart Van Assche <bvanassche@acm.org>
2018-11-10 17:47:10 +0000

    srpt/conftest/bad_wr_mod/bad_wr_mod.c: Avoid that Coverity complains about NULL pointer dereferences
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@7805 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- srpt/conftest/bad_wr_mod/bad_wr_mod.c
+++ srpt/conftest/bad_wr_mod/bad_wr_mod.c
@@ -3,11 +3,11 @@
 
 static int modinit(void)
 {
-	struct ib_qp *qp = NULL;
-	const struct ib_recv_wr *recv_wr = NULL;
-	const struct ib_recv_wr **bad_recv_wr = NULL;
+	struct ib_qp qp = { };
+	const struct ib_recv_wr recv_wr = { };
+	const struct ib_recv_wr *bad_recv_wr;
 
-	return ib_post_recv(qp, recv_wr, bad_recv_wr);
+	return ib_post_recv(&qp, &recv_wr, &bad_recv_wr);
 }
 
 module_init(modinit);
