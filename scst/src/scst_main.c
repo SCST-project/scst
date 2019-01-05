457d6fceed56f28d5af36fc7aaccbe051d3cefe7
Bart Van Assche <bvanassche@acm.org>
2019-01-05 21:42:33 +0000

    scst, scst_vdisk: Simplify the code for querying the vdisk filename
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@7860 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- scst/src/scst_main.c
+++ scst/src/scst_main.c
@@ -1307,8 +1307,6 @@ static void scst_unregister_device(struct scsi_device *scsidp)
 		goto out_unlock;
 	}
 
-	dev->dev_unregistering = 1;
-
 	list_del_init(&dev->dev_list_entry);
 
 #ifdef CONFIG_SCST_FORWARD_MODE_PASS_THROUGH
@@ -1535,8 +1533,7 @@ out:
 	return res;
 
 out_unreg:
-	dev->dev_unregistering = 1;
-	list_del(&dev->dev_list_entry);
+	list_del_init(&dev->dev_list_entry);
 	scst_assign_dev_handler(dev, &scst_null_devtype);
 	goto out_pr_clear_dev;
 
@@ -1592,8 +1589,6 @@ void scst_unregister_virtual_device(int id,
 		PRINT_ERROR("Virtual device (id %d) not found", id);
 		goto out_unlock;
 	}
-
-	dev->dev_unregistering = 1;
 
 	scst_cm_on_dev_unregister(dev);
 
