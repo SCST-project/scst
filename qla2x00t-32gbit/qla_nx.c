bfeb9ad911898e4a47e75498414b611d68a79176
Bart Van Assche <bvanassche@acm.org>
2019-05-01 22:38:41 +0000

    qla2xxx-32gbit: Increase the size of the mailbox arrays from 4 to 8
    This patch avoids that Coverity complains that qla2x00_async_event() writes
    outside the bounds of the mb[] arrays (MBA_IDC_AEN case).
    
    
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@8293 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- qla2x00t-32gbit/qla_nx.c
+++ qla2x00t-32gbit/qla_nx.c
@@ -2030,7 +2030,7 @@ qla82xx_intr_handler(int irq, void *dev_id)
 	unsigned long	flags;
 	unsigned long	iter;
 	uint32_t	stat = 0;
-	uint16_t	mb[4];
+	uint16_t	mb[8];
 
 	rsp = (struct rsp_que *) dev_id;
 	if (!rsp) {
@@ -2114,7 +2114,7 @@ qla82xx_msix_default(int irq, void *dev_id)
 	unsigned long flags;
 	uint32_t stat = 0;
 	uint32_t host_int = 0;
-	uint16_t mb[4];
+	uint16_t mb[8];
 
 	rsp = (struct rsp_que *) dev_id;
 	if (!rsp) {
@@ -2210,7 +2210,7 @@ qla82xx_poll(int irq, void *dev_id)
 	int status = 0;
 	uint32_t stat;
 	uint32_t host_int = 0;
-	uint16_t mb[4];
+	uint16_t mb[8];
 	unsigned long flags;
 
 	rsp = (struct rsp_que *) dev_id;
