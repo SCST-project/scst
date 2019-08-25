72cc459a40552823d6862a260ebbe0c56e0c2ad9
Bart Van Assche <bvanassche@acm.org>
2019-08-25 02:36:13 +0000

    qla2x00t-32gbit: Complain if sp->done() is not called from the completion path
    Not calling sp->done() from the command completion path is a severe bug.
    Hence complain loudly if that happens.
    
    [ commit 88263208dd23327b56fbd7c2858c389c1153b366 upstream ]
    
    
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@8548 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- qla2x00t-32gbit/qla_mr.c
+++ qla2x00t-32gbit/qla_mr.c
@@ -2554,6 +2554,8 @@ check_scsi_status:
 
 	if (rsp->status_srb == NULL)
 		sp->done(sp, res);
+	else
+		WARN_ON_ONCE(true);
 }
 
 /**
@@ -2631,6 +2633,8 @@ qlafx00_status_cont_entry(struct rsp_que *rsp, sts_cont_entry_t *pkt)
 	if (sense_len == 0) {
 		rsp->status_srb = NULL;
 		sp->done(sp, cp->result);
+	} else {
+		WARN_ON_ONCE(true);
 	}
 }
 
