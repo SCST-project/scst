18879e695ffb477bcda6114fb15eabd06074539d
Bart Van Assche <bvanassche@acm.org>
2019-11-03 00:16:12 +0000

    qla2x00t-32gbit, qla_bsg.c: Minimize diffs with the upstream code
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@8625 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- qla2x00t-32gbit/qla_bsg.c
+++ qla2x00t-32gbit/qla_bsg.c
@@ -384,7 +384,7 @@ qla2x00_process_els(struct bsg_job *bsg_job)
 
 	rsp_sg_cnt = dma_map_sg(&ha->pdev->dev, bsg_job->reply_payload.sg_list,
 		bsg_job->reply_payload.sg_cnt, DMA_FROM_DEVICE);
-	if (!rsp_sg_cnt) {
+        if (!rsp_sg_cnt) {
 		dma_unmap_sg(&ha->pdev->dev, bsg_job->reply_payload.sg_list,
 		    bsg_job->reply_payload.sg_cnt, DMA_FROM_DEVICE);
 		rval = -ENOMEM;
