SCST-project/scst

commit 5ebd20d839d0b32a1ec53ebcf68c0030f40bc8f9

scst/include/backport.h: Backport cpu_to_be32_array()
git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@8838 d57e44dd-8a1f-0410-8b47-8ef2f437770f

scst/include/backport.h
@@ -182,6 +182,21 @@ static inline void *bsg_job_sense(struct bsg_job *job)
 #endif
 #endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 31) */
 
+/* <linux/byteorder/generic.h> */
+/*
+ * See also f2f2efb807d3 ("byteorder: Move {cpu_to_be32, be32_to_cpu}_array()
+ * from Thunderbolt to core") # v4.15.
+ */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0)
+static inline void cpu_to_be32_array(__be32 *dst, const u32 *src, size_t len)
+{
+	int i;
+
+	for (i = 0; i < len; i++)
+		dst[i] = cpu_to_be32(src[i]);
+}
+#endif
+
 /* <linux/compiler.h> */
 
 #if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 20)

commit f4ce28b5abbcfaf2aeab5eedee861543d404786d

scst/include/backport.h: Unbreak the build on RHEL 7 / CentOS 7
Fixes: 5ebd20d839d0 ("scst/include/backport.h: Backport cpu_to_be32_array()")


git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@8879 d57e44dd-8a1f-0410-8b47-8ef2f437770f

scst/include/backport.h
@@ -187,7 +187,8 @@ static inline void *bsg_job_sense(struct bsg_job *job)
  * See also f2f2efb807d3 ("byteorder: Move {cpu_to_be32, be32_to_cpu}_array()
  * from Thunderbolt to core") # v4.15.
  */
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 15, 0) && \
+	(!defined(RHEL_MAJOR) || RHEL_MAJOR -0 < 7)
 static inline void cpu_to_be32_array(__be32 *dst, const u32 *src, size_t len)
 {
 	int i;