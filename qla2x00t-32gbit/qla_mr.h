ca607d88ddaf34a9d9397c853d5be820ca77fd15
Bart Van Assche <bvanassche@acm.org>
2020-08-02 21:53:19 +0000

    qla2x00t-32gbit: Change two fxdisc_entry_fx00 members from a scalar into an array
    This patch does not change any functionality but suppresses a Coverity
    warning.
    
    
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@9092 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- qla2x00t-32gbit/qla_mr.h
+++ qla2x00t-32gbit/qla_mr.h
@@ -177,8 +177,8 @@ struct fxdisc_entry_fx00 {
 	uint8_t flags;
 	uint8_t reserved_1;
 
-	struct dsd64 dseg_rq;
-	struct dsd64 dseg_rsp;
+	struct dsd64 dseg_rq[1];
+	struct dsd64 dseg_rsp[1];
 
 	__le32 dataword;
 	__le32 adapid;
