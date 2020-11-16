1686029f602992cf597ebdad45b5dc9f7280da01
Bart Van Assche <bvanassche@acm.org>
2020-11-16 04:34:24 +0000

    fcst: Port to Linux kernel v5.10
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@9181 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- fcst/ft_io.c
+++ fcst/ft_io.c
@@ -19,10 +19,15 @@
  * You should have received a copy of the GNU General Public License along with
  * this program.
  */
+#include <linux/version.h>
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <scsi/libfc.h>
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 10, 0)
 #include <scsi/fc_encode.h>
+#else
+#include <scsi/fc_frame.h>
+#endif
 #include "fcst.h"
 
 /*
