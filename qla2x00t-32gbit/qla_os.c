7e12fdac01e3be8fa57ab09f5a55c505d82b4654
Bart Van Assche <bvanassche@acm.org>
2020-12-28 17:54:03 +0000

    qla2x00t-32gbit: Minimize diffs with the upstream code
    See also upstream commit 62e9dd177732 ("scsi: qla2xxx: Change in PUREX to
    handle FPIN ELS requests").
    
    
    
    git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@9247 d57e44dd-8a1f-0410-8b47-8ef2f437770f

--- qla2x00t-32gbit/qla_os.c
+++ qla2x00t-32gbit/qla_os.c
@@ -6379,7 +6379,8 @@ dealloc:
 		    rsp_els, rsp_els_dma);
 }
 
-void qla24xx_free_purex_item(struct purex_item *item)
+void
+qla24xx_free_purex_item(struct purex_item *item)
 {
 	if (item == &item->vha->default_item)
 		memset(&item->vha->default_item, 0, sizeof(struct purex_item));
