SCST-project/scst

commit 8eeb9af89117a306e389bcafcc2e4d180117cc18

scst: Fix the CentOS / RHEL 8.[012] builds
git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@9328 d57e44dd-8a1f-0410-8b47-8ef2f437770f

scst/include/backport.h
@@ -1229,7 +1229,8 @@ static inline int pcie_capability_read_dword(struct pci_dev *dev, int pos,
 #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 3, 0)
-#if !defined(RHEL_MAJOR) || RHEL_MAJOR -0 < 8
+#if defined(RHEL_RELEASE_CODE) &&				\
+	 RHEL_RELEASE_CODE -0 >= RHEL_RELEASE_VERSION(8, 3))
 /*
  * See also commit 09ed79d6d75f ("percpu_ref: introduce PERCPU_REF_ALLOW_REINIT
  * flag") # v5.3.

commit 013aecf10e48605cb248a1ad54c125ee032b9af9

scst: Fix the PERCPU_REF_ALLOW_REINIT backport
Fixes: 8eeb9af89117 ("scst: Fix the CentOS / RHEL 8.[012] builds")


git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@9329 d57e44dd-8a1f-0410-8b47-8ef2f437770f

scst/include/backport.h
@@ -1228,15 +1228,14 @@ static inline int pcie_capability_read_dword(struct pci_dev *dev, int pos,
 #include <linux/percpu-refcount.h>
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 3, 0)
-#if defined(RHEL_RELEASE_CODE) &&				\
-	 RHEL_RELEASE_CODE -0 >= RHEL_RELEASE_VERSION(8, 3))
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 3, 0) &&	\
+	(!defined(RHEL_RELEASE_CODE) ||				\
+	 RHEL_RELEASE_CODE -0 < RHEL_RELEASE_VERSION(8, 3))
 /*
  * See also commit 09ed79d6d75f ("percpu_ref: introduce PERCPU_REF_ALLOW_REINIT
  * flag") # v5.3.
  */
 #define PERCPU_REF_ALLOW_REINIT 0
-#endif
 #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 17, 0)