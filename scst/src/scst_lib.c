SCST-project/scst

commit 4525b04b23f310d3defbcca2f637f78d9b901a7b

scst: Reject inconsistent COMPARE AND WRITE commands
Reject COMPARE AND WRITE commands if the size of the data buffer does not
match the number of logical blocks specified in the CDB. This patch makes
two more libiscsi conformance tests pass.


git-svn-id: http://svn.code.sf.net/p/scst/svn/trunk@8927 d57e44dd-8a1f-0410-8b47-8ef2f437770f

scst/src/scst_lib.c
@@ -6870,6 +6870,19 @@ out_finish:
 	goto out;
 }
 
+/*
+ * The Data-Out Buffer contains two instances of logical block data:
+ * 1) the compare instance, in which:
+ *   A) the user data is used for the compare operation; and
+ *   B) the protection information, if any, is not used;
+ * and
+ * 2) the write instance, in which:
+ *   A) the user data is used for the write operations; and
+ *   B) the protection information, if any, is used for the write operations.
+ *
+ * Note: cmd->data_len is half of the DataOut buffer size while cmd->bufflen
+ * and cmd->expected_transfer_len refer to the entire DataOut buffer.
+ */
 enum scst_exec_res scst_cmp_wr_local(struct scst_cmd *cmd)
 {
 	enum scst_exec_res res = SCST_EXEC_COMPLETED;
@@ -6912,6 +6925,14 @@ enum scst_exec_res scst_cmp_wr_local(struct scst_cmd *cmd)
 
 	cwrp->cwr_orig_cmd = cmd;
 	cwrp->cwr_finish_fn = scst_cwr_read_cmd_finished;
+
+	if (cmd->bufflen != scst_cmd_get_expected_transfer_len_data(cmd)) {
+		PRINT_ERROR("COMPARE AND WRITE: data buffer length mismatch (CDB %u <> ini %u)",
+			    cmd->bufflen,
+			    scst_cmd_get_expected_transfer_len_data(cmd));
+		scst_set_invalid_field_in_cdb(cmd, 13/*NLB*/, 0);
+		goto out_done;
+	}
 
 	/*
 	 * As required by SBC, DIF PI, if any, is not checked for the read part

commit 67c89ae0f97c5b2027be8cc73e7054feecb4f60c

scst_lib: Fix memory leak reported by Coverity
This patch fixes the following Coverity complaint:

    CID 275306 (#1 of 1): Resource leak (RESOURCE_LEAK)
    Variable cwrp going out of scope leaks the storage it points to.

In the scst_cmp_wr_local() function, we don't free 'cwrp' if the next
check after the memory allocation fails. Hence move this check before
allocating memory.

Fixes: 4525b04b2 ("scst: Reject inconsistent COMPARE AND WRITE commands")

scst/src/scst_lib.c
@@ -6906,6 +6906,14 @@ enum scst_exec_res scst_cmp_wr_local(struct scst_cmd *cmd)
 		goto out_done;
 	}
 
+	if (cmd->bufflen != scst_cmd_get_expected_transfer_len_data(cmd)) {
+		PRINT_ERROR("COMPARE AND WRITE: data buffer length mismatch (CDB %u <> ini %u)",
+			    cmd->bufflen,
+			    scst_cmd_get_expected_transfer_len_data(cmd));
+		scst_set_invalid_field_in_cdb(cmd, 13/*NLB*/, 0);
+		goto out_done;
+	}
+
 	/* ToDo: HWALIGN'ed kmem_cache */
 	cwrp = kzalloc(sizeof(*cwrp), GFP_KERNEL);
 	if (cwrp == NULL) {
@@ -6916,14 +6924,6 @@ enum scst_exec_res scst_cmp_wr_local(struct scst_cmd *cmd)
 
 	cwrp->cwr_orig_cmd = cmd;
 	cwrp->cwr_finish_fn = scst_cwr_read_cmd_finished;
-
-	if (cmd->bufflen != scst_cmd_get_expected_transfer_len_data(cmd)) {
-		PRINT_ERROR("COMPARE AND WRITE: data buffer length mismatch (CDB %u <> ini %u)",
-			    cmd->bufflen,
-			    scst_cmd_get_expected_transfer_len_data(cmd));
-		scst_set_invalid_field_in_cdb(cmd, 13/*NLB*/, 0);
-		goto out_done;
-	}
 
 	/*
 	 * As required by SBC, DIF PI, if any, is not checked for the read part