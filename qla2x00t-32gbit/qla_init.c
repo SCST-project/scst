917b58000f8fdb12bbd4477301284aac5ec869b9
Gleb Chesnokov <gleb.chesnokov@scst.dev>
2022-12-15 10:46:11 +0300

    qla2x00t-32gbit: Remove unused variable 'found_devs'
    Variable 'found_devs' is just being incremented and it's never used
    anywhere else. Remove it.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>
    Link: https://lore.kernel.org/r/20221101104733.30363-1-colin.i.king@gmail.com
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    [ commit e137b81d30e7 upstream ]

--- qla2x00t-32gbit/qla_init.c
+++ qla2x00t-32gbit/qla_init.c
@@ -5547,7 +5547,6 @@ static int
 qla2x00_configure_local_loop(scsi_qla_host_t *vha)
 {
 	int		rval, rval2;
-	int		found_devs;
 	int		found;
 	fc_port_t	*fcport, *new_fcport;
 	uint16_t	index;
@@ -5562,7 +5561,6 @@ qla2x00_configure_local_loop(scsi_qla_host_t *vha)
 	if (N2N_TOPO(ha))
 		return qla2x00_configure_n2n_loop(vha);
 
-	found_devs = 0;
 	new_fcport = NULL;
 	entries = MAX_FIBRE_DEVICES_LOOP;
 
@@ -5721,8 +5719,6 @@ qla2x00_configure_local_loop(scsi_qla_host_t *vha)
 
 		/* Base iIDMA settings on HBA port speed. */
 		fcport->fp_speed = ha->link_data_rate;
-
-		found_devs++;
 	}
 
 	list_for_each_entry(fcport, &vha->vp_fcports, list) {
