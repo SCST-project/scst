SCST-project/scst

commit e8b300ef2af29b1d0b761e82b1c18cd5eed32bd2

usr/fileio/common.c: Fix use of uninitialized struct field
This patch fixes the following Coverity complaint:

    CID 275308 (#1-2 of 2): Uninitialized scalar variable (UNINIT)
    uninit_use_in_call: Using uninitialized value
    multi.multi_cmd.replies_done when calling fprintf.

usr/fileio/common.c
@@ -792,7 +792,7 @@ void *main_loop(void *arg)
 		struct scst_user_reply_cmd replies[MULTI_CMDS_CNT];
 		struct scst_user_get_multi multi_cmd;
 		struct scst_user_get_cmd cmds[MULTI_CMDS_CNT];
-	} multi;
+	} multi = {};
 
 	TRACE_ENTRY();
 

commit b6d1f1943771925f2acc96e7c74f8a13b11d85c6

usr/fileio/common.c: Fix warning for older versions of GCC
Building SCST with an old version of GCC (e.g. 4.8.5 on Centos 7.X)
produces a warning about using initializer.

This is GCC bug # 53119:
  https://gcc.gnu.org/bugzilla/show_bug.cgi?id=53119

Hence, use memset() instead of an initializer to avoid this warning.

Fixes: e8b300ef2af2 ("usr/fileio/common.c: Fix use of uninitialized
struct field")

usr/fileio/common.c
@@ -792,7 +792,9 @@ void *main_loop(void *arg)
 		struct scst_user_reply_cmd replies[MULTI_CMDS_CNT];
 		struct scst_user_get_multi multi_cmd;
 		struct scst_user_get_cmd cmds[MULTI_CMDS_CNT];
-	} multi = {};
+	} multi;
+
+	memset(&multi, 0, sizeof(multi));
 
 	TRACE_ENTRY();
 