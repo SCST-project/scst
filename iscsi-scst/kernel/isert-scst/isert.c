1171841931cec25d62cb1c23e1bb351e565d015c
Gleb Chesnokov <gleb.chesnokov@scst.dev>
2024-01-12 18:50:40 +0300

    Bump the version number to 3.8.0
    These changes have been generated by running the following command:
    
    $ scripts/update-version 3 8 0

--- iscsi-scst/kernel/isert-scst/isert.c
+++ iscsi-scst/kernel/isert-scst/isert.c
@@ -503,8 +503,8 @@ out:
 MODULE_AUTHOR("Yan Burman");
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_IMPORT_NS(SCST);
-#define DRV_VERSION		"3.8.0-pre" "#" __stringify(OFED_FLAVOR)
-#define DRV_RELDATE		"28 December 2022"
+#define DRV_VERSION		"3.8.0" "#" __stringify(OFED_FLAVOR)
+#define DRV_RELDATE		"12 January 2024"
 MODULE_DESCRIPTION("iSER target transport driver "
 		   "v" DRV_VERSION " (" DRV_RELDATE ")");
 module_init(isert_init_module);
