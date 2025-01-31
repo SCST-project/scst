146d79ec88da1a7d01725d3f8805000fb8751520
Brian M <brian.meagher@ixsystems.com>
2025-01-31 09:41:06 -0800

    iscsi-scst: Add SHA256 and SHA3-256 support to CHAP
    Use the kernel userspace AL_ALG API to access additional hash
    functions "sha256" and "sha3-256".
    
    If configured for CHAP, on iSCSI login the client will present an
    ordered list of desired algorithms.  During this negotiation, if
    the client requests SHA256 or SHA3-256 we verify that the kernel
    supports the algorithm before agreeing to use it.

---
+++ iscsi-scst/usr/af_alg.h
@@ -0,0 +1,30 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ *  Copyright (C) 2025 Brian Meagher <brian.meagher@ixsystems.com>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation, version 2
+ *  of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ */
+
+#ifndef SCST_AF_ALG_H
+#define SCST_AF_ALG_H
+
+#include <unistd.h>
+#include <stdbool.h>
+
+#define SCST_AF_ALG_SHA256_NAME  "sha256"
+#define SCST_AF_ALG_SHA3_256_NAME  "sha3-256"
+
+int af_alg_init(const char *algorithm);
+void af_alg_update(int datafd, const void *data_in, size_t len);
+ssize_t af_alg_final(int datafd, void *out, size_t len);
+bool af_alg_supported(char *alg);
+
+#endif
