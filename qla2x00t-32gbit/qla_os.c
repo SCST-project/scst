SCST-project/scst

commit 0e8fdad5e48c15b98c29470a991aaa71a4147b56

scst: Use sysfs_emit/sysfs_emit_at instead of scnprintf()
Replace scnprintf() with sysfs_emit() and sysfs_emit_at() in sysfs show handlers.

These helper functions are specifically designed for sysfs output, providing safer
handling of buffer lengths and consistency across kernel sysfs interfaces.

This patch does not change any functionality.

scst/include/backport.h
@@ -37,8 +37,11 @@
 #include <linux/iocontext.h>
 #include <linux/kobject_ns.h>
 #include <linux/scatterlist.h>	/* struct scatterlist */
+#include <linux/shrinker.h>
 #include <linux/slab.h>		/* kmalloc() */
 #include <linux/stddef.h>	/* sizeof_field() */
+#include <linux/string.h>
+#include <linux/sysfs.h>
 #include <linux/timer.h>
 #include <linux/vmalloc.h>
 #include <linux/workqueue.h>
@@ -1563,6 +1566,79 @@ static inline ssize_t strscpy(char *dest, const char *src, size_t count)
 	(!defined(RHEL_MAJOR) || RHEL_MAJOR -0 < 7)
 /* See also commit b9b3259746d7 ("sysfs.h: add __ATTR_RW() macro") # v3.11. */
 #define __ATTR_RW(_name) __ATTR(_name, 0644, _name##_show, _name##_store)
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 10, 0) &&			\
+	(LINUX_VERSION_CODE >> 8 != KERNEL_VERSION(4, 9, 0) >> 8 ||	\
+	 LINUX_VERSION_CODE < KERNEL_VERSION(4, 9, 260)) &&		\
+	(LINUX_VERSION_CODE >> 8 != KERNEL_VERSION(4, 14, 0) >> 8 ||	\
+	 LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 224)) &&		\
+	(LINUX_VERSION_CODE >> 8 != KERNEL_VERSION(4, 19, 0) >> 8 ||	\
+	 LINUX_VERSION_CODE < KERNEL_VERSION(4, 19, 179)) &&		\
+	(LINUX_VERSION_CODE >> 8 != KERNEL_VERSION(5, 4, 0) >> 8 ||	\
+	 LINUX_VERSION_CODE < KERNEL_VERSION(5, 4, 103))
+/*
+ * See also commit 2efc459d06f1 ("sysfs: Add sysfs_emit and sysfs_emit_at to format sysfs output")
+ * # v5.10.
+ * See also commit f3c3dcf35532 # v4.9.260.
+ * See also commit 390881843b4f # v4.14.224.
+ * See also commit cb1f69d53ac8 # v4.19.179.
+ * See also commit 5f4243642873 # v5.4.103.
+ */
+static inline __printf(2, 3)
+int sysfs_emit(char *buf, const char *fmt, ...)
+{
+	va_list args;
+	int len;
+
+	if (WARN(!buf ||
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 20, 0)
+		/*
+		 * For kernel releases older than 4.20, using the SLUB allocator will cause
+		 * this alignment check to fail as that allocator did NOT align kmalloc
+		 * allocations on a PAGE_SIZE boundary.
+		 */
+		 false,
+#else
+		 offset_in_page(buf),
+#endif
+		 "invalid sysfs_emit: buf:%p\n", buf))
+		return 0;
+
+	va_start(args, fmt);
+	len = vscnprintf(buf, PAGE_SIZE, fmt, args);
+	va_end(args);
+
+	return len;
+}
+
+static inline __printf(3, 4)
+int sysfs_emit_at(char *buf, int at, const char *fmt, ...)
+{
+	va_list args;
+	int len;
+
+	if (WARN(!buf ||
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 20, 0)
+		/*
+		 * For kernel releases older than 4.20, using the SLUB allocator will cause
+		 * this alignment check to fail as that allocator did NOT align kmalloc
+		 * allocations on a PAGE_SIZE boundary.
+		 */
+		 false ||
+#else
+		 offset_in_page(buf) ||
+#endif
+		 at < 0 || at >= PAGE_SIZE,
+		 "invalid sysfs_emit_at: buf:%p at:%d\n", buf, at))
+		return 0;
+
+	va_start(args, fmt);
+	len = vscnprintf(buf + at, PAGE_SIZE - at, fmt, args);
+	va_end(args);
+
+	return len;
+}
 #endif
 
 /* <linux/t10-pi.h> */

iscsi-scst/kernel/config.c
@@ -29,22 +29,22 @@ static ssize_t iscsi_version_show(struct kobject *kobj, struct kobj_attribute *a
 
 	TRACE_ENTRY();
 
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret, "%s\n", ISCSI_VERSION_STRING);
+	ret += sysfs_emit_at(buf, ret, "%s\n", ISCSI_VERSION_STRING);
 
 #ifdef CONFIG_SCST_EXTRACHECKS
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret, "EXTRACHECKS\n");
+	ret += sysfs_emit_at(buf, ret, "EXTRACHECKS\n");
 #endif
 
 #ifdef CONFIG_SCST_TRACING
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret, "TRACING\n");
+	ret += sysfs_emit_at(buf, ret, "TRACING\n");
 #endif
 
 #ifdef CONFIG_SCST_DEBUG
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret, "DEBUG\n");
+	ret += sysfs_emit_at(buf, ret, "DEBUG\n");
 #endif
 
 #ifdef CONFIG_SCST_ISCSI_DEBUG_DIGEST_FAILURES
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret, "DEBUG_DIGEST_FAILURES\n");
+	ret += sysfs_emit_at(buf, ret, "DEBUG_DIGEST_FAILURES\n");
 #endif
 
 	TRACE_EXIT();
@@ -60,16 +60,16 @@ static ssize_t iscsi_open_state_show(struct kobject *kobj, struct kobj_attribute
 
 	switch (ctr_open_state) {
 	case ISCSI_CTR_OPEN_STATE_CLOSED:
-		ret = scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "closed\n");
+		ret = sysfs_emit(buf, "closed\n");
 		break;
 	case ISCSI_CTR_OPEN_STATE_OPEN:
-		ret = scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "open\n");
+		ret = sysfs_emit(buf, "open\n");
 		break;
 	case ISCSI_CTR_OPEN_STATE_CLOSING:
-		ret = scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "closing\n");
+		ret = sysfs_emit(buf, "closing\n");
 		break;
 	default:
-		ret = scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "unknown\n");
+		ret = sysfs_emit(buf, "unknown\n");
 		break;
 	}
 
@@ -442,7 +442,7 @@ static ssize_t iscsi_attr_show(struct kobject *kobj, struct kobj_attribute *attr
 	if (pos != 0)
 		goto out;
 
-	pos = scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%s\n", (char *)value);
+	pos = sysfs_emit(buf, "%s\n", (char *)value);
 
 	kfree(value);
 

iscsi-scst/kernel/conn.c
@@ -35,43 +35,43 @@ static struct lockdep_map scst_conn_dep_map =
 	STATIC_LOCKDEP_MAP_INIT("iscsi_conn_kref", &scst_conn_key);
 #endif
 
-static int print_conn_state(char *p, size_t size, struct iscsi_conn *conn)
+static ssize_t print_conn_state(char *buf, size_t size, struct iscsi_conn *conn)
 {
-	int pos = 0;
+	ssize_t ret = 0;
 
 	if (conn->closing) {
-		pos += scnprintf(p, size, "closing");
+		ret += scnprintf(buf, size, "closing");
 		goto out;
 	}
 
 	switch (conn->rd_state) {
 	case ISCSI_CONN_RD_STATE_PROCESSING:
-		pos += scnprintf(&p[pos], size - pos, "read_processing ");
+		ret += scnprintf(buf + ret, size - ret, "read_processing ");
 		break;
 	case ISCSI_CONN_RD_STATE_IN_LIST:
-		pos += scnprintf(&p[pos], size - pos, "in_read_list ");
+		ret += scnprintf(buf + ret, size - ret, "in_read_list ");
 		break;
 	}
 
 	switch (conn->wr_state) {
 	case ISCSI_CONN_WR_STATE_PROCESSING:
-		pos += scnprintf(&p[pos], size - pos, "write_processing ");
+		ret += scnprintf(buf + ret, size - ret, "write_processing ");
 		break;
 	case ISCSI_CONN_WR_STATE_IN_LIST:
-		pos += scnprintf(&p[pos], size - pos, "in_write_list ");
+		ret += scnprintf(buf + ret, size - ret, "in_write_list ");
 		break;
 	case ISCSI_CONN_WR_STATE_SPACE_WAIT:
-		pos += scnprintf(&p[pos], size - pos, "space_waiting ");
+		ret += scnprintf(buf + ret, size - ret, "space_waiting ");
 		break;
 	}
 
 	if (test_bit(ISCSI_CONN_REINSTATING, &conn->conn_aflags))
-		pos += scnprintf(&p[pos], size - pos, "reinstating ");
-	else if (pos == 0)
-		pos += scnprintf(&p[pos], size - pos, "established idle ");
+		ret += scnprintf(buf + ret, size - ret, "reinstating ");
+	else if (ret == 0)
+		ret += scnprintf(buf + ret, size - ret, "established idle ");
 
 out:
-	return pos;
+	return ret;
 }
 
 static void iscsi_conn_release(struct kobject *kobj)
@@ -98,17 +98,17 @@ static ssize_t iscsi_get_initiator_ip(struct iscsi_conn *conn, char *buf, int si
 
 static ssize_t iscsi_conn_ip_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
-	int pos;
 	struct iscsi_conn *conn;
+	ssize_t ret;
 
 	TRACE_ENTRY();
 
 	conn = container_of(kobj, struct iscsi_conn, conn_kobj);
 
-	pos = iscsi_get_initiator_ip(conn, buf, SCST_SYSFS_BLOCK_SIZE);
+	ret = iscsi_get_initiator_ip(conn, buf, SCST_SYSFS_BLOCK_SIZE);
 
-	TRACE_EXIT_RES(pos);
-	return pos;
+	TRACE_EXIT_RES(ret);
+	return ret;
 }
 
 static struct kobj_attribute iscsi_conn_ip_attr =
@@ -116,8 +116,8 @@ static struct kobj_attribute iscsi_conn_ip_attr =
 
 static ssize_t iscsi_get_target_ip(struct iscsi_conn *conn, char *buf, int size)
 {
-	int pos;
 	struct sock *sk;
+	ssize_t ret;
 
 	TRACE_ENTRY();
 
@@ -127,40 +127,40 @@ static ssize_t iscsi_get_target_ip(struct iscsi_conn *conn, char *buf, int size)
 	sk = conn->sock->sk;
 	switch (sk->sk_family) {
 	case AF_INET:
-		pos = scnprintf(buf, size, "%pI4", &inet_sk(sk)->inet_saddr);
+		ret = scnprintf(buf, size, "%pI4", &inet_sk(sk)->inet_saddr);
 		break;
 #ifdef CONFIG_IPV6
 	case AF_INET6:
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 13, 0)
-		pos = scnprintf(buf, size, "[%pI6]", &inet6_sk(sk)->saddr);
+		ret = scnprintf(buf, size, "[%pI6]", &inet6_sk(sk)->saddr);
 #else
-		pos = scnprintf(buf, size, "[%pI6]", &sk->sk_v6_rcv_saddr);
+		ret = scnprintf(buf, size, "[%pI6]", &sk->sk_v6_rcv_saddr);
 #endif
 #endif
 		break;
 	default:
-		pos = scnprintf(buf, size, "Unknown family %d", sk->sk_family);
+		ret = scnprintf(buf, size, "Unknown family %d", sk->sk_family);
 		break;
 	}
 
-	TRACE_EXIT_RES(pos);
-	return pos;
+	TRACE_EXIT_RES(ret);
+	return ret;
 }
 
 static ssize_t iscsi_conn_target_ip_show(struct kobject *kobj, struct kobj_attribute *attr,
 					 char *buf)
 {
-	int pos;
 	struct iscsi_conn *conn;
+	ssize_t ret;
 
 	TRACE_ENTRY();
 
 	conn = container_of(kobj, struct iscsi_conn, conn_kobj);
 
-	pos = iscsi_get_target_ip(conn, buf, SCST_SYSFS_BLOCK_SIZE);
+	ret = iscsi_get_target_ip(conn, buf, SCST_SYSFS_BLOCK_SIZE);
 
-	TRACE_EXIT_RES(pos);
-	return pos;
+	TRACE_EXIT_RES(ret);
+	return ret;
 }
 
 static struct kobj_attribute iscsi_conn_target_ip_attr =
@@ -176,7 +176,7 @@ static ssize_t iscsi_conn_transport_show(struct kobject *kobj, struct kobj_attri
 
 	conn = container_of(kobj, struct iscsi_conn, conn_kobj);
 
-	ret = scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%s\n", conn->transport->name);
+	ret = sysfs_emit(buf, "%s\n", conn->transport->name);
 
 	TRACE_EXIT_RES(ret);
 	return ret;
@@ -194,7 +194,7 @@ static ssize_t iscsi_conn_cid_show(struct kobject *kobj, struct kobj_attribute *
 
 	conn = container_of(kobj, struct iscsi_conn, conn_kobj);
 
-	ret = scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%u", conn->cid);
+	ret = sysfs_emit(buf, "%u", conn->cid);
 
 	TRACE_EXIT_RES(ret);
 	return ret;
@@ -205,17 +205,17 @@ static struct kobj_attribute iscsi_conn_cid_attr =
 
 static ssize_t iscsi_conn_state_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
-	int pos;
 	struct iscsi_conn *conn;
+	ssize_t ret;
 
 	TRACE_ENTRY();
 
 	conn = container_of(kobj, struct iscsi_conn, conn_kobj);
 
-	pos = print_conn_state(buf, SCST_SYSFS_BLOCK_SIZE, conn);
+	ret = print_conn_state(buf, SCST_SYSFS_BLOCK_SIZE, conn);
 
-	TRACE_EXIT_RES(pos);
-	return pos;
+	TRACE_EXIT_RES(ret);
+	return ret;
 }
 
 static struct kobj_attribute iscsi_conn_state_attr =

iscsi-scst/kernel/session.c
@@ -388,8 +388,8 @@ static ssize_t iscsi_sess_show_##name(struct kobject *kobj,			\
 	scst_sess = container_of(kobj, struct scst_session, sess_kobj);		\
 	sess = (struct iscsi_session *)scst_sess_get_tgt_priv(scst_sess);	\
 										\
-	return scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%s\n",			\
-			 iscsi_get_bool_value(sess->sess_params.name));		\
+	return sysfs_emit(buf, "%s\n",						\
+			  iscsi_get_bool_value(sess->sess_params.name));	\
 }										\
 										\
 static struct kobj_attribute iscsi_sess_attr_##name =				\
@@ -405,8 +405,8 @@ static ssize_t iscsi_sess_show_##name(struct kobject *kobj,			\
 	scst_sess = container_of(kobj, struct scst_session, sess_kobj);		\
 	sess = (struct iscsi_session *)scst_sess_get_tgt_priv(scst_sess);	\
 										\
-	return scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%d\n",			\
-			 sess->sess_params.name);				\
+	return sysfs_emit(buf, "%d\n",						\
+			  sess->sess_params.name);				\
 }										\
 										\
 static struct kobj_attribute iscsi_sess_attr_##name =				\
@@ -423,8 +423,8 @@ static ssize_t iscsi_sess_show_##name(struct kobject *kobj,				\
 	scst_sess = container_of(kobj, struct scst_session, sess_kobj);			\
 	sess = (struct iscsi_session *)scst_sess_get_tgt_priv(scst_sess);		\
 											\
-	return scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%s\n",				\
-			 iscsi_get_digest_name(sess->sess_params.name, digest_name));	\
+	return sysfs_emit(buf, "%s\n",							\
+			  iscsi_get_digest_name(sess->sess_params.name, digest_name));	\
 }											\
 											\
 static struct kobj_attribute iscsi_sess_attr_##name =					\
@@ -451,7 +451,7 @@ static ssize_t iscsi_sess_sid_show(struct kobject *kobj, struct kobj_attribute *
 	scst_sess = container_of(kobj, struct scst_session, sess_kobj);
 	sess = (struct iscsi_session *)scst_sess_get_tgt_priv(scst_sess);
 
-	ret = scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%llx\n", sess->sid);
+	ret = sysfs_emit(buf, "%llx\n", sess->sid);
 
 	TRACE_EXIT_RES(ret);
 	return ret;
@@ -472,7 +472,7 @@ static ssize_t iscsi_sess_reinstating_show(struct kobject *kobj, struct kobj_att
 	scst_sess = container_of(kobj, struct scst_session, sess_kobj);
 	sess = (struct iscsi_session *)scst_sess_get_tgt_priv(scst_sess);
 
-	ret = scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%d\n", sess->sess_reinstating ? 1 : 0);
+	ret = sysfs_emit(buf, "%d\n", sess->sess_reinstating ? 1 : 0);
 
 	TRACE_EXIT_RES(ret);
 	return ret;
@@ -497,11 +497,11 @@ static ssize_t iscsi_sess_thread_pid_show(struct kobject *kobj, struct kobj_attr
 
 	mutex_lock(&thr_pool->tp_mutex);
 	list_for_each_entry(t, &thr_pool->threads_list, threads_list_entry)
-		res += scnprintf(buf + res, SCST_SYSFS_BLOCK_SIZE - res, "%d%s",
-				 task_pid_vnr(t->thr),
-				 list_is_last(&t->threads_list_entry,
-					      &thr_pool->threads_list) ?
-				 "\n" : " ");
+		res += sysfs_emit_at(buf, res, "%d%s",
+				     task_pid_vnr(t->thr),
+				     list_is_last(&t->threads_list_entry,
+						  &thr_pool->threads_list) ?
+				     "\n" : " ");
 	mutex_unlock(&thr_pool->tp_mutex);
 
 out:

iscsi-scst/kernel/target.c
@@ -404,7 +404,7 @@ static ssize_t iscsi_tgt_tid_show(struct kobject *kobj, struct kobj_attribute *a
 	if (!tgt)
 		goto out;
 
-	res = scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%u\n", tgt->tid);
+	res = sysfs_emit(buf, "%u\n", tgt->tid);
 
 out:
 	TRACE_EXIT_RES(res);
@@ -577,8 +577,8 @@ static ssize_t iscsi_acg_sess_dedicated_threads_show(struct kobject *kobj,
 	acg = container_of(kobj, struct scst_acg, acg_kobj);
 	dedicated = scst_get_acg_tgt_priv(acg);
 
-	ret = scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%d\n%s",
-			dedicated, dedicated ? SCST_SYSFS_KEY_MARK "\n" : "");
+	ret = sysfs_emit(buf, "%d\n%s",
+			 dedicated, dedicated ? SCST_SYSFS_KEY_MARK "\n" : "");
 
 	TRACE_EXIT_RES(ret);
 	return ret;

qla2x00t-32gbit/qla2x00-target/scst_qla2xxx.c
@@ -895,28 +895,27 @@ static ssize_t sqa_version_show(struct kobject *kobj,
 {
 	ssize_t ret = 0;
 
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret,
-			 "INTERFACE=%s\nSCST=%s\nQLOGIC=%s\n",
-			 SQA_VERSION, SCST_VERSION_NAME, QLA2XXX_VERSION);
+	ret += sysfs_emit_at(buf, ret, "INTERFACE=%s\nSCST=%s\nQLOGIC=%s\n",
+			     SQA_VERSION, SCST_VERSION_NAME, QLA2XXX_VERSION);
 
 #ifdef CONFIG_SCST_EXTRACHECKS
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret, "EXTRACHECKS\n");
+	ret += sysfs_emit_at(buf, ret, "EXTRACHECKS\n");
 #endif
 
 #ifdef CONFIG_SCST_TRACING
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret, "TRACING\n");
+	ret += sysfs_emit_at(buf, ret, "TRACING\n");
 #endif
 
 #ifdef CONFIG_SCST_DEBUG
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret, "DEBUG\n");
+	ret += sysfs_emit_at(buf, ret, "DEBUG\n");
 #endif
 
 #ifdef CONFIG_QLA_TGT_DEBUG_WORK_IN_THREAD
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret, "QLA_TGT_DEBUG_WORK_IN_THREAD\n");
+	ret += sysfs_emit_at(buf, ret, "QLA_TGT_DEBUG_WORK_IN_THREAD\n");
 #endif
 
 #ifdef CONFIG_QLA_TGT_DEBUG_SRR
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret, "DEBUG_SRR\n");
+	ret += sysfs_emit_at(buf, ret, "DEBUG_SRR\n");
 #endif
 
 	TRACE_EXIT();
@@ -935,8 +934,7 @@ static ssize_t sqa_hw_target_show(struct kobject *kobj,
 
 	tgt = sqa_tgt->qla_tgt;
 
-	return scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%d\n",
-			 tgt->vha->vp_idx == 0 ? 1 : 0);
+	return sysfs_emit(buf, "%d\n", tgt->vha->vp_idx == 0 ? 1 : 0);
 }
 
 static ssize_t sqa_node_name_show(struct kobject *kobj,
@@ -973,11 +971,10 @@ static ssize_t sqa_node_name_show(struct kobject *kobj,
 	if (res != 0)
 		goto out;
 
-	res = scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%s\n", wwn);
+	res = sysfs_emit(buf, "%s\n", wwn);
 
 	if (ha->tgt.node_name_set)
-		res += scnprintf(buf + res, SCST_SYSFS_BLOCK_SIZE - res, "%s\n",
-				 SCST_SYSFS_KEY_MARK);
+		res += sysfs_emit_at(buf, res, "%s\n", SCST_SYSFS_KEY_MARK);
 
 	kfree(wwn);
 
@@ -1072,7 +1069,7 @@ static ssize_t sqa_vp_parent_host_show(struct kobject *kobj,
 	if (res != 0)
 		goto out;
 
-	res = scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%s\n%s\n", wwn, SCST_SYSFS_KEY_MARK);
+	res = sysfs_emit(buf, "%s\n%s\n", wwn, SCST_SYSFS_KEY_MARK);
 
 	kfree(wwn);
 
@@ -1095,10 +1092,10 @@ static ssize_t sqa_show_expl_conf_enabled(struct kobject *kobj,
 	tgt = sqa_tgt->qla_tgt;
 	ha = tgt->ha;
 
-	return scnprintf(buffer, SCST_SYSFS_BLOCK_SIZE, "%d\n%s",
-			 ha->base_qpair->enable_explicit_conf,
-			 ha->base_qpair->enable_explicit_conf ?
-			 SCST_SYSFS_KEY_MARK "\n" : "");
+	return sysfs_emit(buffer, "%d\n%s",
+			  ha->base_qpair->enable_explicit_conf,
+			  ha->base_qpair->enable_explicit_conf ?
+			  SCST_SYSFS_KEY_MARK "\n" : "");
 }
 
 static ssize_t sqa_store_expl_conf_enabled(struct kobject *kobj,

qla2x00t/qla2x00-target/qla2x00t.c
@@ -6601,8 +6601,8 @@ out:
 }
 
 
-static ssize_t q2t_show_expl_conf_enabled(struct kobject *kobj,
-	struct kobj_attribute *attr, char *buffer)
+static ssize_t q2t_show_expl_conf_enabled(struct kobject *kobj, struct kobj_attribute *attr,
+					  char *buffer)
 {
 	struct scst_tgt *scst_tgt;
 	struct q2t_tgt *tgt;
@@ -6613,11 +6613,12 @@ static ssize_t q2t_show_expl_conf_enabled(struct kobject *kobj,
 	tgt = scst_tgt_get_tgt_priv(scst_tgt);
 	if (!tgt)
 		goto out;
+
 	vha = tgt->vha;
 
-	res = scnprintf(buffer, PAGE_SIZE, "%d\n%s",
-		    vha->hw->enable_explicit_conf,
-		    vha->hw->enable_explicit_conf ? SCST_SYSFS_KEY_MARK "\n" : "");
+	res = sysfs_emit(buffer, "%d\n%s",
+			 vha->hw->enable_explicit_conf,
+			 vha->hw->enable_explicit_conf ? SCST_SYSFS_KEY_MARK "\n" : "");
 
 out:
 	return res;
@@ -6709,22 +6710,22 @@ static ssize_t q2t_version_show(struct kobject *kobj, struct kobj_attribute *att
 {
 	size_t ret = 0;
 
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret, "%s\n", Q2T_VERSION_STRING);
+	ret += sysfs_emit_at(buf, ret, "%s\n", Q2T_VERSION_STRING);
 
 #ifdef CONFIG_SCST_EXTRACHECKS
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret, "EXTRACHECKS\n");
+	ret += sysfs_emit_at(buf, ret, "EXTRACHECKS\n");
 #endif
 
 #ifdef CONFIG_SCST_TRACING
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret, "TRACING\n");
+	ret += sysfs_emit_at(buf, ret, "TRACING\n");
 #endif
 
 #ifdef CONFIG_SCST_DEBUG
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret, "DEBUG\n");
+	ret += sysfs_emit_at(buf, ret, "DEBUG\n");
 #endif
 
 #ifdef CONFIG_QLA_TGT_DEBUG_WORK_IN_THREAD
-	ret += scnprintf(buf + ret, SCST_SYSFS_BLOCK_SIZE - ret, "QLA_TGT_DEBUG_WORK_IN_THREAD\n");
+	ret += sysfs_emit_at(buf, ret, "QLA_TGT_DEBUG_WORK_IN_THREAD\n");
 #endif
 
 	TRACE_EXIT();
@@ -6733,7 +6734,7 @@ static ssize_t q2t_version_show(struct kobject *kobj, struct kobj_attribute *att
 
 static ssize_t q2t_hw_target_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
-	return scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%d\n", 1);
+	return sysfs_emit(buf, "%d\n", 1);
 }
 
 static ssize_t q2t_node_name_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
@@ -6757,12 +6758,11 @@ static ssize_t q2t_node_name_show(struct kobject *kobj, struct kobj_attribute *a
 	if (res != 0)
 		goto out;
 
-	res = scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%s\n", wwn);
+	res = sysfs_emit(buf, "%s\n", wwn);
 
 	/* For virtual ports it's always key */
 	if (vha->node_name_set || (vha->vp_idx != 0))
-		res += scnprintf(buf + res, SCST_SYSFS_BLOCK_SIZE - res, "%s\n",
-				 SCST_SYSFS_KEY_MARK);
+		res += sysfs_emit_at(buf, res, "%s\n", SCST_SYSFS_KEY_MARK);
 
 	kfree(wwn);
 
@@ -6860,12 +6860,11 @@ static ssize_t q2t_port_name_show(struct kobject *kobj, struct kobj_attribute *a
 	if (res != 0)
 		goto out;
 
-	res = scnprintf(buf, "%s\n", wwn);
+	res = sysfs_emit(buf, "%s\n", wwn);
 
 	/* For virtual ports it's always key */
 	if ((vha->vp_idx != 0) || vha->port_name_set)
-		res += scnprintf(buf + res, SCST_SYSFS_BLOCK_SIZE - res, "%s\n",
-				 SCST_SYSFS_KEY_MARK);
+		res += sysfs_emit_at(buf, res, "%s\n", SCST_SYSFS_KEY_MARK);
 
 	kfree(wwn);
 
@@ -6958,7 +6957,7 @@ static ssize_t q2t_vp_parent_host_show(struct kobject *kobj, struct kobj_attribu
 	if (res != 0)
 		goto out;
 
-	res = scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%s\n%s\n", wwn, SCST_SYSFS_KEY_MARK);
+	res = sysfs_emit(buf, "%s\n%s\n", wwn, SCST_SYSFS_KEY_MARK);
 
 	kfree(wwn);
 

scst/src/dev_handlers/scst_disk.c
@@ -565,9 +565,9 @@ static ssize_t disk_sysfs_cluster_mode_show(struct kobject *kobj, struct kobj_at
 	struct scst_device *dev = container_of(kobj, struct scst_device,
 					       dev_kobj);
 
-	return scnprintf(buf, SCST_SYSFS_BLOCK_SIZE, "%d\n%s",
-			 dev->cluster_mode,
-			 dev->cluster_mode ? SCST_SYSFS_KEY_MARK "\n" : "");
+	return sysfs_emit(buf, "%d\n%s",
+			  dev->cluster_mode,
+			  dev->cluster_mode ? SCST_SYSFS_KEY_MARK "\n" : "");
 }
 
 static int disk_sysfs_process_cluster_mode_store(struct scst_sysfs_work_item *work)

commit 157a4864ad680efa252ba80ecf7c031bd9302cdd

scst: Fix backport for stable, RHEL, and UEK kernels
Update backport logic to support recent stable, RHEL, and UEK kernel
versions. Adjust version checks and compatibility for sysfs_emit,
timer_delete, and related helpers.

Fixes: 0e8fdad5e48c ("scst: Use sysfs_emit/sysfs_emit_at instead of scnprintf()")
Fixes: d21b6f1457f1 ("scst: Port to Linux kernel v6.15")

qla2x00t-32gbit/qla_os.c
@@ -2949,7 +2949,14 @@ static void qla2x00_iocb_work_fn(struct work_struct *work)
 static void
 qla_trace_init(void)
 {
-#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 8, 0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 8, 0) &&			\
+	(LINUX_VERSION_CODE >> 8 != KERNEL_VERSION(6, 6, 0) >> 8 ||	\
+	 LINUX_VERSION_CODE < KERNEL_VERSION(6, 6, 87))
+	/*
+	 * See also commit d23569979ca1 ("tracing: Allow creating instances with specified system
+	 * events") # v6.8.
+	 * See also commit f568fbe8c603 # v6.6.87.
+	 */
 	qla_trc_array = trace_array_get_by_name("qla2xxx");
 #else
 	qla_trc_array = trace_array_get_by_name("qla2xxx", NULL);

scst/include/backport.h
@@ -43,6 +43,7 @@
 #include <linux/string.h>
 #include <linux/sysfs.h>
 #include <linux/timer.h>
+#include <linux/usb/quirks.h>
 #include <linux/vmalloc.h>
 #include <linux/workqueue.h>
 #include <linux/writeback.h>	/* sync_page_range() */
@@ -1498,7 +1499,8 @@ static inline void __user *KERNEL_SOCKPTR(void *p)
 /* <linux/string.h> */
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 5, 0) &&	\
-	(!defined(RHEL_MAJOR) || RHEL_MAJOR -0 < 7)
+	(!defined(RHEL_MAJOR) || RHEL_MAJOR -0 < 7) &&	\
+	(!defined(UEK_KABI_RENAME) || !defined(USB_QUIRK_NO_LPM))
 /* See also commit e9d408e107db ("new helper: memdup_user_nul()") # v4.5 */
 static inline void *memdup_user_nul(const void __user *src, size_t len)
 {
@@ -1576,7 +1578,10 @@ static inline ssize_t strscpy(char *dest, const char *src, size_t count)
 	(LINUX_VERSION_CODE >> 8 != KERNEL_VERSION(4, 19, 0) >> 8 ||	\
 	 LINUX_VERSION_CODE < KERNEL_VERSION(4, 19, 179)) &&		\
 	(LINUX_VERSION_CODE >> 8 != KERNEL_VERSION(5, 4, 0) >> 8 ||	\
-	 LINUX_VERSION_CODE < KERNEL_VERSION(5, 4, 103))
+	 LINUX_VERSION_CODE < KERNEL_VERSION(5, 4, 103)) &&		\
+	(!defined(RHEL_RELEASE_CODE) ||					\
+	 RHEL_RELEASE_CODE -0 < RHEL_RELEASE_VERSION(8, 5)) &&		\
+	!defined(UEK_KABI_RENAME)
 /*
  * See also commit 2efc459d06f1 ("sysfs: Add sysfs_emit and sysfs_emit_at to format sysfs output")
  * # v5.10.
@@ -1664,6 +1669,9 @@ struct t10_pi_tuple {
 	 LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 154)) &&		\
 	(LINUX_VERSION_CODE >> 8 != KERNEL_VERSION(6, 1, 0) >> 8 ||	\
 	 LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 84)) &&		\
+	(!defined(RHEL_MAJOR) || RHEL_MAJOR -0 < 8 ||			\
+	 RHEL_MAJOR -0 == 8 && RHEL_MINOR -0 < 9 ||			\
+	 RHEL_MAJOR -0 == 9 && RHEL_MINOR -0 < 3) &&			\
 	(!defined(UEK_KABI_RENAME) ||					\
 	 LINUX_VERSION_CODE < KERNEL_VERSION(5, 4, 17))
 /*
@@ -1683,7 +1691,10 @@ static inline int timer_delete_sync(struct timer_list *timer)
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(6, 2, 0) &&			\
 	(LINUX_VERSION_CODE >> 8 != KERNEL_VERSION(6, 1, 0) >> 8 ||	\
-	 LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 91))
+	 LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 91)) &&		\
+	(!defined(RHEL_MAJOR) || RHEL_MAJOR -0 < 8 ||			\
+	 RHEL_MAJOR -0 == 8 && RHEL_MINOR -0 < 9 ||			\
+	 RHEL_MAJOR -0 == 9 && RHEL_MINOR -0 < 3)
 /*
  * See also commit bb663f0f3c39 ("timers: Rename del_timer() to timer_delete()") # v6.2.
  * See also commit b086d1e82fcd # v6.1.91.